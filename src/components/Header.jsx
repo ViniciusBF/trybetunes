7bcbe4d0532eec26e4cfd53e647f578e9768cc72
Vinícius <viniciusgolfera@gmail.com>
2022-07-01 18:04:36 -0300

    requisito 4 atendido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 
@@ -21,6 +22,17 @@ class Header extends React.Component {
     this.setState({ user: name, isLoading: false });
   }
 
+  renderHeader = (user) => (
+    <>
+      <p data-testid="header-user-name">{ `Olá ${user}` }</p>
+      <nav>
+        <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+      </nav>
+    </>
+  )
+
   render() {
     const { user, isLoading } = this.state;
     return (
@@ -28,7 +40,7 @@ class Header extends React.Component {
         {
           isLoading
             ? <Carregando />
-            : <p data-testid="header-user-name">{ `Olá ${user}` }</p>
+            : this.renderHeader(user)
         }
       </header>
     );
