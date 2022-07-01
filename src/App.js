535162c2507108632e6fd50c1e6d0576ab9e3d69
Vinícius <viniciusgolfera@gmail.com>
2022-07-01 15:11:36 -0300

    requisito 1 atendido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,79 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <div data-testid="page-login">
+                <Login />
+              </div>
+            ) }
+          />
+          <Route
+            path="/search"
+            render={ () => (
+              <div data-testid="page-search">
+                <Search />
+              </div>
+            ) }
+          />
+          <Route
+            path="/album/:id"
+            render={ () => (
+              <div data-testid="page-album">
+                <Album />
+              </div>
+            ) }
+          />
+          <Route
+            path="/favorites"
+            render={ () => (
+              <div data-testid="page-favorites">
+                <Favorites />
+              </div>
+            ) }
+          />
+          <Route
+            path="/profile/edit"
+            render={ () => (
+              <div data-testid="page-profile-edit">
+                <ProfileEdit />
+              </div>
+            ) }
+          />
+          <Route
+            path="/profile"
+            render={ () => (
+              <div data-testid="page-profile">
+                <Profile />
+              </div>
+            ) }
+          />
+          <Route
+            path="*"
+            render={ () => (
+              <div data-testid="page-not-found">
+                <NotFound />
+              </div>
+            ) }
+          />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
